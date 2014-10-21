e8437a1d13f0cef2d221210348df5098b714186d
charlie <charlie@speakeasy2.dutcherdesigns.com>
2014-10-21 15:36:50 -0700

    Switched back map.py to my version

--- map.py
+++ map.py
@@ -3,28 +3,8 @@
 import index
 
 def print_content():
-    print("""
-<style type="text/css">
-	.draggable{
-		position:relative;
-		cursor: move;
-	}
-
-	#map{
-		width: 1000px;
-		height: 700px;
-		background-color: darkgrey;
-	}
-</style>
-<script src="map/mapping.js"></script>
-""")
-
-    print('<table border=4><tr><th style="overflow: hidden;">')
-    print('<div id="map">')
-    print("<img id='mapImage' src='map/someRandomMap.jpg' class='draggable' style='top:-500px;left:-500px;zoom=1' />")
-    #print("<img src='http://3.bp.blogspot.com/--Qn8gNCWlUc/UVhKBl5o5aI/AAAAAAAACYI/wMLgckCYQIs/s1600/Screen+Shot+2013-03-31+at+12.59.10+AM.png' class='draggable' />")
-    print('</div>')
-    print('</th></tr></table>')
+    with open("map/map.html", "r") as fin:
+        print(fin.read())
 
 if __name__ == '__main__':
     index.main(print_content)
