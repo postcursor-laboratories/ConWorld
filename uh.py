56450fc512f788ba169ecde662869cf0af76264a
Kenzie Togami <ket1999@gmail.com>
2014-10-21 07:13:06 -0700

    Break everything on bad parsers

--- uh.py
+++ uh.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
-print("Content-type: text/plain\n\nHELLO FOOL")
+print("""Content-type: text/plain
+
+<b>my bad formatting brings all the browsers to the yard
+<ul><li>one<li>two<li>three""")
