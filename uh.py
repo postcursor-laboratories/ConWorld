bc2bac4365485f78e5bae6566d5a5c06adecd2de
Kenzie Togami <ket1999@gmail.com>
2014-10-21 10:14:05 -0700

    Use index module here

--- uh.py
+++ uh.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-print("""Content-type: text/plain
+
+import index
+
+def print_content():
+    print("""Content-type: text/plain
 
 <b>my bad formatting brings all the browsers to the yard
 <ul><li>one<li>two<li>three""")
+
+if __name__ == '__main__':
+    index.main(print_content)
