5847179c34a4a3011bb4189e0abdca87c2625374
charlie <charlie@speakeasy2.dutcherdesigns.com>
2014-10-21 15:31:35 -0700

    work on the map

--- map/mapping.js
+++ map/mapping.js
@@ -1,4 +1,5 @@
 // Credit to https://stackoverflow.com/questions/17992543/how-do-i-drag-an-image-smoothly-around-the-screen-using-pure-javascript
+// for the dragging code.
 
 function drag_getTarget(e){
     // IE uses srcElement, sanity uses target
@@ -7,7 +8,7 @@ function drag_getTarget(e){
 }
 
 function drag_init(e) {
-    console.log("drag_init("+e+")");
+    //console.log("drag_init("+e+")");
 
     // determine event object
     if (!e) {
@@ -15,18 +16,20 @@ function drag_init(e) {
     }
 
     var targ = drag_getTarget(e);
-    if (targ.className != 'draggable') {return};
+    if (targ.className != 'draggable') {
+	return
+    }
 
     // calculate event X, Y coordinates
-    offsetX = e.clientX;
-    offsetY = e.clientY;
+    clickInitialX = e.clientX;
+    clickInitialY = e.clientY;
 
-    console.log(targ);
-    console.log(targ.style);
-    console.log("LEFT: "+targ.style.left);
+    //console.log(targ);
+    //console.log(targ.style);
+    //console.log("LEFT: "+targ.style.left);
     // assign default values for top and left properties
-    //if(!targ.style.left) { targ.style.left = '0px'};
-    //if(!targ.style.top)  { targ.style.top  = '0px'};
+    if(!targ.style.left) { targ.style.left = '0px'};
+    if(!targ.style.top)  { targ.style.top  = '0px'};
 
     // calculate integer values for top and left properties
     coordX = parseInt(targ.style.left);
@@ -39,7 +42,7 @@ function drag_init(e) {
 }
 
 function drag_drag(e) {
-    console.log("drag_drag("+e+")");
+    //console.log("drag_drag("+e+")");
 
     if (!drag_isDragging) {return};
     if (!e) {
@@ -50,27 +53,46 @@ function drag_drag(e) {
 
     var shouldMove = true;
     /* // TODO: boundary conditions
-    if(coordX < 0 && offsetX < 0)
+    if(coordX < 0 && clickInitialX < 0)
 	shouldMove = true;
-    if(targ.height + offsetY > 0)
+    if(targ.height + clickInitialY > 0)
 	shouldMove = true;
     */
 
     // move element
     if(shouldMove){
-	targ.style.left = coordX+e.clientX-offsetX+'px';
-	targ.style.top  = coordY+e.clientY-offsetY+'px';
+	targ.style.left = coordX+e.clientX-clickInitialX+'px';
+	targ.style.top  = coordY+e.clientY-clickInitialY+'px';
 	//targ.style.zoom += e.scroll;
 	return false;
     }
 }
 
 function drag_stop() {
-    console.log("drag_stop()");
+    //console.log("drag_stop()");
     drag_isDragging=false;
 }
 
 window.onload = function() {
     document.onmousedown = drag_init;
     document.onmouseup   = drag_stop;
+}
+
+// ====================================================================== end dragging code
+
+function tile_name(x, y){
+    if(x < 0 || x >= 2048) return 'invalid';
+    if(y < 0 || y >= 2048) return 'invalid';
+
+    var xval = ('0000'+x).slice(-4);
+    var yval = ('0000'+y).slice(-4);
+    return "tile-"+xval+"-"+yval;
+}
+
+function change_tile_loadedness(x,y,loaded){
+    var name = tile_name(x,y);
+    var elem = document.getElementById(name);
+    console.log(elem);
+    var text = loaded ? '<img src="map/'+name+'.png" />' : '';
+    elem.innerHTML = text;
 }
