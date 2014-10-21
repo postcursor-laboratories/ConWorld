b30621deaea487938849ee6df803514778f39536
charlie <charlie@speakeasy2.dutcherdesigns.com>
2014-10-20 23:54:55 -0700

    Work on getting the map to work nicely

--- map_playground.py
+++ map_playground.py
@@ -9,13 +9,21 @@ def print_content():
 		position:relative;
 		cursor: move;
 	}
-</style>
-<script src="mapping.js" />
-""")                            # WHY CAN'T I INCLUDE THIS SCRIPT AARHGH
 
-    print('<table border=4><tr><th>')
-    #print("<img src='someRandomMap.jpg' class='draggable' />")
-    print("<img src='http://3.bp.blogspot.com/--Qn8gNCWlUc/UVhKBl5o5aI/AAAAAAAACYI/wMLgckCYQIs/s1600/Screen+Shot+2013-03-31+at+12.59.10+AM.png' />")
+	#map{
+		width: 1000px;
+		height: 700px;
+		background-color: darkgrey;
+	}
+</style>
+<script src="mapping.js"></script>
+""")
+
+    print('<table border=4><tr><th style="overflow: hidden;">')
+    print('<div id="map">')
+    print("<img id='mapImage' src='someRandomMap.jpg' class='draggable' style='top:-500px;left:-500px;zoom=1' />")
+    #print("<img src='http://3.bp.blogspot.com/--Qn8gNCWlUc/UVhKBl5o5aI/AAAAAAAACYI/wMLgckCYQIs/s1600/Screen+Shot+2013-03-31+at+12.59.10+AM.png' class='draggable' />")
+    print('</div>')
     print('</th></tr></table>')
 
 if __name__ == '__main__':
--- mapping.js
+++ mapping.js
@@ -1,47 +1,76 @@
-function startDrag(e) {
+// Credit to https://stackoverflow.com/questions/17992543/how-do-i-drag-an-image-smoothly-around-the-screen-using-pure-javascript
+
+function drag_getTarget(e){
+    // IE uses srcElement, sanity uses target
+    var targ = e.target ? e.target : e.srcElement;
+    return targ;
+}
+
+function drag_init(e) {
+    console.log("drag_init("+e+")");
+
     // determine event object
     if (!e) {
         var e = window.event;
     }
-    // IE uses srcElement, others use target
-    var targ = e.target ? e.target : e.srcElement;
+
+    var targ = drag_getTarget(e);
     if (targ.className != 'draggable') {return};
 
     // calculate event X, Y coordinates
     offsetX = e.clientX;
     offsetY = e.clientY;
 
+    console.log(targ);
+    console.log(targ.style);
+    console.log("LEFT: "+targ.style.left);
     // assign default values for top and left properties
-    if(!targ.style.left) { targ.style.left='0px'};
-    if (!targ.style.top) { targ.style.top='0px'};
+    //if(!targ.style.left) { targ.style.left = '0px'};
+    //if(!targ.style.top)  { targ.style.top  = '0px'};
 
     // calculate integer values for top and left properties
     coordX = parseInt(targ.style.left);
     coordY = parseInt(targ.style.top);
-    drag = true;
+    drag_isDragging = true;
 
-    // move div element
-    document.onmousemove=dragDiv;
-    turn false;
-}
-
-function dragDiv(e) {
-    if (!drag) {return};
-    if (!e) { var e=window.event};
-
-    var targ=e.target?e.target:e.srcElement;
-
-    // move div element
-    targ.style.left=coordX+e.clientX-offsetX+'px';
-    targ.style.top=coordY+e.clientY-offsetY+'px';
+    // move element
+    document.onmousemove=drag_drag;
     return false;
 }
 
-function stopDrag() {
-    drag=false;
+function drag_drag(e) {
+    console.log("drag_drag("+e+")");
+
+    if (!drag_isDragging) {return};
+    if (!e) {
+	var e=window.event
+    }
+
+    var targ = drag_getTarget(e);
+
+    var shouldMove = true;
+    /* // TODO: boundary conditions
+    if(coordX < 0 && offsetX < 0)
+	shouldMove = true;
+    if(targ.height + offsetY > 0)
+	shouldMove = true;
+    */
+
+    // move element
+    if(shouldMove){
+	targ.style.left = coordX+e.clientX-offsetX+'px';
+	targ.style.top  = coordY+e.clientY-offsetY+'px';
+	//targ.style.zoom += e.scroll;
+	return false;
+    }
+}
+
+function drag_stop() {
+    console.log("drag_stop()");
+    drag_isDragging=false;
 }
 
 window.onload = function() {
-    document.onmousedown = startDrag;
-    document.onmouseup = stopDrag;
+    document.onmousedown = drag_init;
+    document.onmouseup   = drag_stop;
 }
