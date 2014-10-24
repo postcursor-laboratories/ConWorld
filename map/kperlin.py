3a8c6f8363ec89d92c4aa510e6a7b696eb1c3f66
Kenzie Togami <ket1999@gmail.com>
2014-10-23 21:41:45 -0700

    kperlin is now part java tmp

--- map/kperlin.py
+++ map/kperlin.py
@@ -1,91 +1,103 @@
-from abc import *
-from numbers import Number
-from collections import Sequence
-import random
-
-def interpolate(a, b, x, xmin=0, xmax=1):
-    return a * (xmax - x) + b * (x - xmin)
-
-class NoiseGenerator2D:
-    __metaclass__  = ABCMeta
-    X1 = 0
-    Y1 = 1
-    X2 = 2
-    Y2 = 3
-    def __init__(self):
-        self.width = 0
-        self.height = 0
-        self.originx = 0
-        self.originy = 0
-        self.noise = {}
-    def __contains__(self, coords):
-        return self.noise.get(coords)
-    def __setgen(self, x, y, gen):
-        self.noise[(x, y)] = gen
-        return gen
-    def getnoise(self, x, y):
-        loc = (x, y)
-        if loc not in self:
-            self.noise[loc] = self.noise2(x, y)
-        return self.noise[loc]
-    @abstractmethod
-    def noise2(self, x, y):
-        pass
-
-class Perlin(NoiseGenerator2D):
-    def __init__(self, seed, octaves=1, persistence=1):
-        super().__init__()
-        self.n = octaves
-        if isinstance(persistence, Number):
-            cache = persistence
-            persistence = lambda i: cache
-        elif isinstance(persistence, Sequence):
-            persistence = persistence.__getitem__
-        s = sum(persistence(i) for i in range(octaves))
-        if s > 1:
-            raise ValueError('persistence values for {} octaves cannot be greater than one, it is {}'.format(octaves, s))
-        self.p = persistence
-        self.seed = seed
-        self.rawnoise = {}
-        self.define_inoise(seed)
-    def define_inoise(self, seed):
-        self.rawnoise = {}
-        self.rawsnoise = {}
-        self.rawinoise = {}
-        def noise(x, y):
-            loc = (x, y)
-            if loc not in self.rawnoise:
-                n = x + y * 0x7fffffff + seed
-                n = (n << 13) ^ n
-                self.rawnoise[loc] = (1.0 - ((n * (n * n *  15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0)
-            return self.rawnoise[loc]
-        def snoise(x, y):
-            loc = (x, y)
-            if loc not in self.rawsnoise:
-                corners = (noise(x - 1, y - 1) + noise(x + 1, y - 1) + noise(x - 1, y + 1) + noise(x + 1, y + 1)) / 16
-                sides   = (noise(x - 1, y) + noise(x + 1, y) + noise(x, y - 1) + noise(x, y + 1)) /  8
-                center  =  noise(x, y) / 4
-                self.rawsnoise[loc] = corners + sides + center
-            return self.rawsnoise[loc]
-        def inoise(x, y):
-            loc = (x, y)
-            if loc not in self.rawinoise:
-                ix = int(x)
-                fx = x - ix
-                iy = int(y)
-                fy = y - iy
-
-                v1 = snoise(ix, iy)
-                v2 = snoise(ix + 1, iy)
-                v3 = snoise(ix, iy + 1)
-                v4 = snoise(ix + 1, iy + 1)
-
-                i1 = interpolate(v1 , v2 , fx)
-                i2 = interpolate(v3 , v4 , fx)
-                self.rawinoise[loc] = interpolate(i1 , i2 , fy)
-            return self.rawinoise[loc]
-        self.inoise = inoise
-    def noise2(self, x, y):
-        return sum(self.inoise(x * 2**i, y * 2**i) * self.p(i)**i for i in range(self.n) if self.p(i) != 0)
-
-__all__ = ["NoiseGenerator2D", "Perlin"]
+public class SimplexNoise { // Simplex noise in 2D, 3D and 4D
+ private static int grad3[][] = {{1,1,0},{-1,1,0},{1,-1,0},{-1,-1,0},
+ {1,0,1},{-1,0,1},{1,0,-1},{-1,0,-1},
+ {0,1,1},{0,-1,1},{0,1,-1},{0,-1,-1}};
+ private static int grad4[][]= {{0,1,1,1}, {0,1,1,-1}, {0,1,-1,1}, {0,1,-1,-1},
+ {0,-1,1,1}, {0,-1,1,-1}, {0,-1,-1,1}, {0,-1,-1,-1},
+ {1,0,1,1}, {1,0,1,-1}, {1,0,-1,1}, {1,0,-1,-1},
+ {-1,0,1,1}, {-1,0,1,-1}, {-1,0,-1,1}, {-1,0,-1,-1},
+ {1,1,0,1}, {1,1,0,-1}, {1,-1,0,1}, {1,-1,0,-1},
+ {-1,1,0,1}, {-1,1,0,-1}, {-1,-1,0,1}, {-1,-1,0,-1},
+ {1,1,1,0}, {1,1,-1,0}, {1,-1,1,0}, {1,-1,-1,0},
+ {-1,1,1,0}, {-1,1,-1,0}, {-1,-1,1,0}, {-1,-1,-1,0}};
+ private static int p[] = {151,160,137,91,90,15,
+ 131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+ 190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+ 88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+ 77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+ 102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+ 135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+ 5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+ 223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+ 129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+ 251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+ 49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+ 138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
+ // To remove the need for index wrapping, double the permutation table length
+ private static int perm[] = new int[512];
+ static { for(int i=0; i<512; i++) perm[i]=p[i & 255]; }
+ // A lookup table to traverse the simplex around a given point in 4D.
+ // Details can be found where this table is used, in the 4D noise method.
+ private static int simplex[][] = {
+ {0,1,2,3},{0,1,3,2},{0,0,0,0},{0,2,3,1},{0,0,0,0},{0,0,0,0},{0,0,0,0},{1,2,3,0},
+ {0,2,1,3},{0,0,0,0},{0,3,1,2},{0,3,2,1},{0,0,0,0},{0,0,0,0},{0,0,0,0},{1,3,2,0},
+ {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},
+ {1,2,0,3},{0,0,0,0},{1,3,0,2},{0,0,0,0},{0,0,0,0},{0,0,0,0},{2,3,0,1},{2,3,1,0},
+ {1,0,2,3},{1,0,3,2},{0,0,0,0},{0,0,0,0},{0,0,0,0},{2,0,3,1},{0,0,0,0},{2,1,3,0},
+ {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},
+ {2,0,1,3},{0,0,0,0},{0,0,0,0},{0,0,0,0},{3,0,1,2},{3,0,2,1},{0,0,0,0},{3,1,2,0},
+ {2,1,0,3},{0,0,0,0},{0,0,0,0},{0,0,0,0},{3,1,0,2},{0,0,0,0},{3,2,0,1},{3,2,1,0}};
+ // This method is a *lot* faster than using (int)Math.floor(x)
+ private static int fastfloor(double x) {
+ return x>0 ? (int)x : (int)x-1;
+ }
+ private static double dot(int g[], double x, double y) {
+ return g[0]*x + g[1]*y; }
+ private static double dot(int g[], double x, double y, double z) {
+ return g[0]*x + g[1]*y + g[2]*z; }
+ private static double dot(int g[], double x, double y, double z, double w) {
+ return g[0]*x + g[1]*y + g[2]*z + g[3]*w; } // 2D simplex noise
+ public static double noise(double xin, double yin) {
+ double n0, n1, n2; // Noise contributions from the three corners
+ // Skew the input space to determine which simplex cell we're in
+ final double F2 = 0.5*(Math.sqrt(3.0)-1.0);
+ double s = (xin+yin)*F2; // Hairy factor for 2D
+ int i = fastfloor(xin+s);
+ int j = fastfloor(yin+s);
+ final double G2 = (3.0-Math.sqrt(3.0))/6.0;
+ double t = (i+j)*G2;
+ double X0 = i-t; // Unskew the cell origin back to (x,y) space
+ double Y0 = j-t;
+ double x0 = xin-X0; // The x,y distances from the cell origin
+ double y0 = yin-Y0;
+ // For the 2D case, the simplex shape is an equilateral triangle.
+ // Determine which simplex we are in.
+ int i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords
+ if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1)
+ else {i1=0; j1=1;} // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+ // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
+ // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
+ // c = (3-sqrt(3))/6
+ double x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
+ double y1 = y0 - j1 + G2;
+ double x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords
+ double y2 = y0 - 1.0 + 2.0 * G2;
+ // Work out the hashed gradient indices of the three simplex corners
+ int ii = i & 255;
+ int jj = j & 255;
+ int gi0 = perm[ii+perm[jj]] % 12;
+ int gi1 = perm[ii+i1+perm[jj+j1]] % 12;
+ int gi2 = perm[ii+1+perm[jj+1]] % 12;
+ // Calculate the contribution from the three corners
+ double t0 = 0.5 - x0*x0-y0*y0;
+ if(t0<0) n0 = 0.0;
+ else {
+ t0 *= t0;
+ n0 = t0 * t0 * dot(grad3[gi0], x0, y0); // (x,y) of grad3 used for 2D gradient
+ }
+ double t1 = 0.5 - x1*x1-y1*y1;
+ if(t1<0) n1 = 0.0;
+ else {
+ t1 *= t1;
+ n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
+ } double t2 = 0.5 - x2*x2-y2*y2;
+ if(t2<0) n2 = 0.0;
+ else {
+ t2 *= t2;
+ n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
+ }
+ // Add contributions from each corner to get the final noise value.
+ // The result is scaled to return values in the interval [-1,1].
+ return 70.0 * (n0 + n1 + n2);
+ }
+ 
