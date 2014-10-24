2411403eeb36827240875e6a739bb3675401fa62
Kenzie Togami <ket1999@gmail.com>
2014-10-23 21:40:10 -0700

    Fixup heightmapper.py

--- map/heightmapper.py
+++ map/heightmapper.py
@@ -2,20 +2,19 @@
 
 import sys
 import png
-import kperlin
+import perlin
 
 def create(octave_weights, seed=None):
-    if not seed:
+    if seed == None:
         seed = 203840           # Keysmash of Grand Worldbuilding
-    basefunction = kperlin.Perlin(seed, len(octave_weights), octave_weights).getnoise
-    return lambda x, y: 127 * basefunction(x, y) + 128
+    return perlin.perlin_noise(seed, octave_weights)
 
 def toPNG(func, xsize, ysize):
     pngdata = []
     for y in range(ysize):
         col = []
         for x in range(xsize):
-            res = func(x, y)
+            res = func(x, y)[0]
             col.append(res)
         pngdata.append(col)
 
@@ -23,26 +22,26 @@ def toPNG(func, xsize, ysize):
     return png.from_array(pngdata, mode="L")
 
 if __name__ == '__main__':
-    if len(sys.argv) not in range(2,5) or '--help' in sys.argv or '-h' in sys.argv:
-        print('2dtexture.py FILE [OCTAVES] [SEED]')
+    if len(sys.argv) < 2 or '--help' in sys.argv or '-h' in sys.argv:
+        print('2dtexture.py FILE [OCTAVES] [SEED] [OCTAVE_ONLY] [OCTAVE_ONLY_NUMBER]')
         print()
         print(__doc__)
-        raise SystemExit
+        exit()
     
     #weights = [.1, .1, .2, .3, .5, 1]
     #weights = [0, 0, 0, 0, 1]
+    octaves = int(sys.argv[2]) if len(sys.argv) > 2 else None
+    seed    = int(sys.argv[3]) if len(sys.argv) > 3 else None
+    octonly = int(sys.argv[4]) if len(sys.argv) > 4 else None
+    octnum  = float(sys.argv[5]) if len(sys.argv) > 5 else None
 
-    weights = [.05, .1, 0, 0, .4, 0, .8]
-
-    if len(sys.argv) > 2:
-        octaves = int(sys.argv[2])
-    else:
-        octaves = len(weights)
-
-    if len(sys.argv) > 3:
-        seed = int(sys.argv[3])
-    else:
-        seed = None
-
-    func = create(weights[:octaves], seed)
-    toPNG(func, 1024, 1024).save(sys.argv[1])
+    weights = [(1/2)**(i+1) for i in range(octaves)]
+    weights = weights[:octaves+1]
+    if octonly:
+        if octonly < len(weights):
+            weights = [weights[i] for i in range(len(weights)) if i == octonly]
+        else:
+            weights = [octnum if i+1 == octonly else 0 for i in range(octonly)]
+    print(weights)
+    func = create(weights, seed)
+    toPNG(func, 256, 256).save(sys.argv[1])
