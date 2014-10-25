9d914e5b4bd92b18f3c20f082611ee79aeeefd7e
Keon Skyfire <kprater3.14@gmail.com>
2014-10-24 22:38:44 -0700

    Pickling stuff for storing worlds

---
+++ map/datamapper.py
@@ -0,0 +1,36 @@
+import pickle 
+import os
+
+class DataMap():
+    """A data map is any metadata about tiles that requires knowledge of the
+    heightmap to generate"""
+    def __init__(self, name):
+        self.name = name
+
+    def generateTile(self, tileX, tileY, heightmap, size=256):
+        pass
+    
+    def loadTile(self, x,y):
+        """Loads a tile from given coordinates"""
+        f = open(os.path.join("data", self.name, str(x) + "-" + str(y) + ".data"), 'rb')
+        tile = pickle.load(f)
+        f.close()
+        return tile
+
+    def saveTile(self, tile,x,y):
+        """Saves a tile as a pickled data file"""
+        path = os.path.join(os.path.dirname(__file__), "data", self.name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        f = open(os.path.join(os.path.dirname(__file__), "data", self.name, (str(x) + "-" + str(y)))+".data", 'wb')
+        pickle.dump(tile, f, 0)
+        f.close()
+
+##DEBUG
+tile = []
+for i in range(1000):
+    tile += [i]
+
+d = DataMap("test")
+d.saveTile(tile, 0,0)
+print(d.loadTile(0,0))
