b26b9424d379ff9075f79261dbee23172503af65
Kenzie Togami <ket1999@gmail.com>
2014-10-25 14:05:37 -0700

    Fixup about.py

--- about.py
+++ about.py
@@ -7,9 +7,9 @@ xml = tag.maker()
 
 def generate_content():
     link = 'http://slatestarcodex.com/2013/04/15/things-i-learned-by-spending-five-thousand-years-in-an-alternate-universe'
-    text1 = (tag.TextTag('ConWorld is a game based on '), xml.a(href=link, 'this random blog post.'))
+    text1 = (tag.TextTag('ConWorld is a game based on '), xml.a(href=link, content='this random blog post.'))
     text2 = tag.TextTag('Live in fear and appreciate the fine majesty of this beautious concoction.')
-    gen = xml.p((text1, xml.br(), text2))
+    return xml.p((text1, xml.br(), text2))
 
 content = generate_content()
 
