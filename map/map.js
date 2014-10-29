01fe82d840fe1c20bc514b6fbffbce4894c54ed2
charlie <charliegreen@berkeley.edu>
2014-10-28 18:42:23 -0700

    Converting map from <table> to Raphael; still in progress. Unloading tiles works, but reloading them doesn't.

--- map/map.js
+++ map/map.js
@@ -1,17 +1,23 @@
-// THE CODE THAT GOVERNS TOPOGRAPHICAL DISPLAYMENT
+// THE CODE THAT GOVERNS TOPOGRAPHICAL AND HIGHLY GEOGRAPHICAL DISPLAYMENT
 
 // -------------------------------------------------------------------------------- globals
 
 // All caps means constant, so don't you be modifying these, ya shifty sneak
 _MOUSE_UP = 0;
 _MOUSE_DN = 1;
+_TILESIZE = 256;
+_METERS_PER_PIXEL = 20;
 
-// Well, except this one. This one is set properly in window.onload below. But begone, sneak!
-_CANVAS = "This hasn't been defined yet! Wait for the page to load!";
+// Well, except these ones. They're set properly in window.onload below. But begone, sneak!
+_RAPHAEL  = "This hasn't been defined yet! Wait for the page to load!";
+_MAPFRAME = "This hasn't been defined yet!";
 
 _mouse_state = _MOUSE_UP;
 _map_x = 0;
 _map_y = 0;
+_mouse_move_lastX = 0;
+_mouse_move_lastY = 0;
+_tile_deletionQueue = [];	// a list of tiles to remove
 
 // -------------------------------------------------------------------------------- mouse handlers
 
@@ -28,154 +34,152 @@ function mouse_move(event){
     }
 
     var pos = mouseCoords(event);
-    console.log(pos, _mouse_state);
+    var deltax = pos.x-_mouse_move_lastX;
+    var deltay = pos.y-_mouse_move_lastY;
 
-    // TODO Set _map_x, _map_y appropriately
+    if (_mouse_state == _MOUSE_DN){
+	_map_x += deltax;
+	_map_y += deltay;
 
-    // Update the map
-    map_draw();
+	// move the map
+	_RAPHAEL.forEach(function(el){
+	    var type = el.node.tagName;
+	    if (type !== "image"){
+		return;
+	    }
+
+	    // el.translate() is deprecated and hella slow anyway
+	    //el.translate(deltax, deltay);
+
+	    // el.transform() is fast, avoids the image lag problem, and
+	    // just.. generally doesn't suck
+	    var x = el.matrix.x(0,0) + deltax;
+	    var y = el.matrix.y(0,0) + deltay;
+	    el.transform("t"+x+","+y);
+
+	    // if tile is no longer visible, remove it
+	    if (!tile_isVisibleEl(el)){
+		if (_tile_deletionQueue.indexOf(el) == -1){
+		    _tile_deletionQueue.push(el); // appends to list
+		    console.log("queue edit: ",_tile_deletionQueue);
+		}
+		return;
+	    }
+	});
+
+	// add the tiles that aren't currently visible
+	/*
+	for (var x = 0; x < parseInt(_RAPHAEL.width); x += _TILESIZE) {
+	    for (var y = 0; y < parseInt(_RAPHAEL.height); y += _TILESIZE) {
+		var els = _RAPHAEL.getElementsByPoint(x,y);
+		//if (els.length != 0) // continue if there's at least one element
+		//continue;
+
+		if (els.length == 0){
+		    // add tile to the map coordinate equivalent of Raphael's screen coords x,y
+		    var mapX = _map_x + x;
+		    var mapY = _map_y + y;
+		    console.log("Adding new tile at ("+mapX+","+mapY+")");
+		    tile_new(mapX, mapY);
+		}else
+		    console.log(0);
+	    }
+	}
+	*/
+
+	// update the metadata bar
+	metabar_update();
+    }
+
+    _mouse_move_lastX = pos.x;
+    _mouse_move_lastY = pos.y;
 }
 
-function mouse_up(event){    _mouse_state = _MOUSE_UP;	}
 function mouse_dn(event){    _mouse_state = _MOUSE_DN;	}
 
-window.onload = function(){
-    var mapFrame = document.getElementById('mapFrame');
-    _CANVAS = Raphael(mapFrame, mapFrame.style.width, mapFrame.style.height, null);
-    //_CANVAS = document.getElementById('canvas');
-    _CANVAS.onmousemove = mouse_move;
-    _CANVAS.onmouseup   = mouse_up;
-    _CANVAS.onmousedown = mouse_dn;
-}
-
-/*
-function mouse_drag(event){
-    console.log(event);
-}
-
-function mouse_dragstart(event){
-    console.log(event);
-}
-
-function mouse_dragend(event){
-    console.log(event);
+function mouse_up(event){
+    _mouse_state = _MOUSE_UP;
+    for (var i=0; i<_tile_deletionQueue.length; i++){
+	var el = _tile_deletionQueue[i];
+	if (!tile_isVisibleEl(el))
+	    el.remove();
+    }
+    _tile_deletionQueue = [];
 }
 
 window.onload = function(){
-    _CANVAS = document.getElementById('canvas');
-    _CANVAS.ondrag = mouse_drag;
-    _CANVAS.ondragstart = mouse_dragstart;
-    _CANVAS.ondragend = mouse_dragend;
+    _MAPFRAME = document.getElementById('mapFrame');
+    _RAPHAEL = Raphael(mapFrame, mapFrame.style.width, mapFrame.style.height, null);
+
+    _MAPFRAME.onmousemove = mouse_move;
+    _MAPFRAME.onmouseup   = mouse_up;
+    _MAPFRAME.onmousedown = mouse_dn;
+
+    map_draw();
 }
-*/
 
 // -------------------------------------------------------------------------------- utilities
 
 function tile_name(x, y){
-    sign = function(n){
+    var sign = function(n){
 	return n>1 ? 1 : -1;
     }
 
+    x /= _TILESIZE;
+    y /= _TILESIZE;
     x = sign(x)*Math.floor(Math.abs(x))
     y = sign(y)*Math.floor(Math.abs(y))
 
     return "tile_"+x+"_"+y;
 }
 
-// -------------------------------------------------------------------------------- drawing
+function tile_isVisibleEl(el){
+    var bbox = el.getBBox();
+    return tile_isVisible(bbox['x'],bbox['y']);
+}
+
+function tile_isVisible(x,y){
+    if (x < -_TILESIZE || y < -_TILESIZE)
+	return false;
+    if (x > parseInt(_RAPHAEL.width) || y > parseInt(_RAPHAEL.height))
+	return false;
+    return true;
+}
+
+function tile_new(x,y){
+    _RAPHAEL.image("map/get_tile.py?n="+tile_name(x,y), x, y, _TILESIZE, _TILESIZE);
+}
+
+// -------------------------------------------------------------------------------- drawing and meta
+
+function metabar_update(){
+    var newx = Math.floor(_map_x*100/_TILESIZE)/100;
+    var newy = Math.floor(_map_y*100/_TILESIZE)/100;
+    var inner = newx+", "+newy;
+
+    /*
+    var fancyCoordsify = function(x){
+	return x/_TILESIZE*20/1000; // one pixel is 20m
+    };
+
+    var x = fancyCoordsify(_map_x);
+    var y = fancyCoordsify(_map_y);
+    
+    var inner = y+"km North, "+x+"km East";
+    */
+
+    document.getElementById('mapPosition').innerHTML = "<pre>"+inner+"</pre>";
+}
 
 function map_draw(){
-    var canvas = document.getElementById('canvas');
-    if(!canvas.getContext){	// just in case?
-	console.log("Couldn't find canvas element with ID `canvas'");
-	return;
-    }
+    // draw the initial map stuff (with Raphael!)
 
-    var g = canvas.getContext('2d');
+    for (var x = _map_x - _TILESIZE; x < _map_x + parseInt(_RAPHAEL.width) + _TILESIZE; x += _TILESIZE) {
+	for (var y = _map_y - _TILESIZE; y < _map_y + parseInt(_RAPHAEL.height) + _TILESIZE; y += _TILESIZE) {
+	    tile_new(x,y);
+	}
+    }    
 
-    // draw stuff
+    var circle = _RAPHAEL.circle(_map_x, _map_y, 20, 20);
+    circle.attr("fill", "#f00");
 }
-
-
-/*
-function change_tile_loadedness(x,y,load){
-    return;
-
-    var name = tile_name(x,y);
-    var elem = document.getElementById(name);
-
-    if (!elem)
-	console.log("Couldn't get elem "+name+":", elem);
-
-    var text = '<img src="map/' + (load ? 'get_tile.py?n='+name : 'unloaded.png') + '" />';
-    var currLoaded = elem.attributes['currentlyLoaded'].value === 'Y';
-
-    if (currLoaded != load){
-	elem.innerHTML = text;
-	elem.attributes['currentlyLoaded'].value = load?'Y':'N';
-    }
-}
-
-function map_move(x,y){
-    // for boundary calculations.
-    var mapFrame = document.getElementById("mapFrame");
-    var mapTable = document.getElementById("mapTable");
-    var minX = -mapTable.clientWidth  + parseInt(mapFrame.style.width);
-    var minY = -mapTable.clientHeight + parseInt(mapFrame.style.height);
-
-    // check boundaries
-    if (x > 0)	x = 0;
-    if (y > 0)	y = 0;
-    if (x < minX)	x = minX;
-    if (y < minY)	y = minY;
-
-    // -----------------------------------------------------------------------------------
-    // move the actual map
-    var targ = document.getElementById("mapTableHolder");
-    targ.style.left = x+'px';
-    targ.style.top  = y+'px';
-    
-    // -----------------------------------------------------------------------------------
-    // load or unload tiles (units in tiles, not pixels)
-    var radius_unload = 4;
-    var radius_load   = 2;
-
-    if(typeof map_move_lastMidTileX === 'undefined')	map_move_lastMidTileX = -1;
-    if(typeof map_move_lastMidTileY === 'undefined')	map_move_lastMidTileY = -1;
-
-    var midTileX = Math.floor(-x/256 + parseInt(mapFrame.style.width)/2/256);
-    var midTileY = Math.floor(-y/256 + parseInt(mapFrame.style.height)/2/256);
-
-    if (midTileX !== map_move_lastMidTileX || midTileY !== map_move_lastMidTileY){
-	for(var i=-radius_unload; i<radius_unload; i++)
-	    for(var j=-radius_unload; j<radius_unload; j++){
-		if (midTileX+i < 0)	continue;
-		if (midTileY+j < 0)	continue;
-		if (midTileX+i >= mapSize)	continue;
-		if (midTileY+j >= mapSize)	continue;
-
-		var load = i*i+j*j < radius_load*radius_load;
-		change_tile_loadedness(midTileX+i, midTileY+j, load);
-	    }
-    }
-
-    map_move_lastMidTileX = midTileX;
-    map_move_lastMidTileY = midTileY;
-
-    // -----------------------------------------------------------------------------------
-    // update metadata
-    
-    var offsetX = -x/256 + parseInt(mapFrame.style.width) /2/256 - mapSize/2;
-    var offsetY = -y/256 + parseInt(mapFrame.style.height)/2/256 - mapSize/2;
-    offsetX = Math.floor(offsetX*100)/100;
-    offsetY = Math.floor(offsetY*100)/100;
-
-    // km? Some invented unit? Furlong-wheelbarrows per square sharkfin? Who knows??
-    var unit = " tiles "; //"<sup>T</sup>";	// "&deg;"
-    offsetX = Math.abs(offsetX)+unit+(offsetX>=0 ? "East"  : "West");
-    offsetY = Math.abs(offsetY)+unit+(offsetY>=0 ? "South" : "North");
-
-    var position = "<pre>"+offsetY+", "+offsetX+"</pre>";
-    document.getElementById("mapPosition").innerHTML = position;
-}
-*/
