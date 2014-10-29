31d556fc2024eb4b81292326bd52c4dea61191fb
charlie <charliegreen@berkeley.edu>
2014-10-28 23:46:58 -0700

    Better metadata bar; added heartbeat function with setInterval instead of handling things inside the mouse event handler, which is pretty slow

--- map/map.js
+++ map/map.js
@@ -15,6 +15,8 @@ _MAPFRAME = "This hasn't been defined yet!";
 _mouse_state = _MOUSE_UP;
 _map_x = 0;
 _map_y = 0;
+_mouse_move_x = 0;
+_mouse_move_y = 0;
 _mouse_move_lastX = 0;
 _mouse_move_lastY = 0;
 _tile_deletionQueue = [];	// a list of tiles to remove
@@ -23,79 +25,26 @@ _tile_deletionQueue = [];	// a list of tiles to remove
 
 function mouse_move(event){
     event = event || window.event;
-    function mouseCoords(ev){
-	if(ev.pageX || ev.pageY){
-	    return {x: ev.pageX, y: ev.pageY};
-	}
-	return {
-	    x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-	    y: ev.clientY + document.body.scrollTop  - document.body.clientTop
-	}
+    if(event.pageX || event.pageY){
+	var pos = { x: event.pageX, y: event.pageY };
+    }else{
+	var pos = {
+	    x: event.clientX + document.body.scrollLeft - document.body.clientLeft,
+	    y: event.clientY + document.body.scrollTop  - document.body.clientTop
+	};
     }
 
-    var pos = mouseCoords(event);
-    var deltax = pos.x-_mouse_move_lastX;
-    var deltay = pos.y-_mouse_move_lastY;
+    _mouse_move_x = pos.x;
+    _mouse_move_y = pos.y;
 
-    if (_mouse_state == _MOUSE_DN){
-	_map_x += deltax;
-	_map_y += deltay;
-
-	// move the map
-	_RAPHAEL.forEach(function(el){
-	    var type = el.node.tagName;
-	    if (type !== "image"){
-		return;
-	    }
-
-	    // el.translate() is deprecated and hella slow anyway
-	    //el.translate(deltax, deltay);
-
-	    // el.transform() is fast, avoids the image lag problem, and
-	    // just.. generally doesn't suck
-	    var x = el.matrix.x(0,0) + deltax;
-	    var y = el.matrix.y(0,0) + deltay;
-	    el.transform("t"+x+","+y);
-
-	    // if tile is no longer visible, remove it
-	    if (!tile_isVisibleEl(el)){
-		if (_tile_deletionQueue.indexOf(el) == -1){
-		    _tile_deletionQueue.push(el); // appends to list
-		    console.log("queue edit: ",_tile_deletionQueue);
-		}
-		return;
-	    }
-	});
-
-	// add the tiles that aren't currently visible
-	/*
-	for (var x = 0; x < parseInt(_RAPHAEL.width); x += _TILESIZE) {
-	    for (var y = 0; y < parseInt(_RAPHAEL.height); y += _TILESIZE) {
-		var els = _RAPHAEL.getElementsByPoint(x,y);
-		//if (els.length != 0) // continue if there's at least one element
-		//continue;
-
-		if (els.length == 0){
-		    // add tile to the map coordinate equivalent of Raphael's screen coords x,y
-		    var mapX = _map_x + x;
-		    var mapY = _map_y + y;
-		    console.log("Adding new tile at ("+mapX+","+mapY+")");
-		    tile_new(mapX, mapY);
-		}else
-		    console.log(0);
-	    }
-	}
-	*/
-
-	// update the metadata bar
-	metabar_update();
-    }
-
-    _mouse_move_lastX = pos.x;
-    _mouse_move_lastY = pos.y;
+    return false;
 }
 
-function mouse_dn(event){    _mouse_state = _MOUSE_DN;	}
+function mouse_dn(event){
+    _mouse_state = _MOUSE_DN;
+
+    return false;
+}
 
 function mouse_up(event){
     _mouse_state = _MOUSE_UP;
@@ -105,6 +54,8 @@ function mouse_up(event){
 	    el.remove();
     }
     _tile_deletionQueue = [];
+
+    return false;
 }
 
 window.onload = function(){
@@ -114,6 +65,16 @@ window.onload = function(){
     _MAPFRAME.onmousemove = mouse_move;
     _MAPFRAME.onmouseup   = mouse_up;
     _MAPFRAME.onmousedown = mouse_dn;
+
+    setInterval(heartbeat, 100);
+    
+    /*
+    setInterval(function(){
+	console.time('heartbeat');
+	heartbeat();
+	console.timeEnd('heartbeat');
+    }, 100);
+    */
 
     map_draw();
 }
@@ -152,21 +113,100 @@ function tile_new(x,y){
 
 // -------------------------------------------------------------------------------- drawing and meta
 
-function metabar_update(){
+// heartbeat() is a function that is called periodically (via setInterval()) to perform updates
+function heartbeat(){
+    var deltax = _mouse_move_x-_mouse_move_lastX;
+    var deltay = _mouse_move_y-_mouse_move_lastY;
+
+    if (_mouse_state == _MOUSE_DN){
+	_map_x += deltax;
+	_map_y += deltay;
+
+	// move the map
+	_RAPHAEL.forEach(function(el){
+	    var type = el.node.tagName;
+	    if (type !== "image"){
+		return;
+	    }
+
+	    // el.translate() is deprecated and hella slow anyway
+	    //el.translate(deltax, deltay);
+
+	    // el.transform() is fast, avoids the image lag problem, and
+	    // just.. generally doesn't suck
+	    var x = el.matrix.x(0,0) + deltax;
+	    var y = el.matrix.y(0,0) + deltay;
+	    el.transform("t"+x+","+y);
+
+	    // if tile is no longer visible, remove it
+	    if (!tile_isVisibleEl(el)){
+		if (_tile_deletionQueue.indexOf(el) == -1){
+		    _tile_deletionQueue.push(el); // appends to list
+		    //console.log("queue edit: ", _tile_deletionQueue);
+		}
+		return;
+	    }
+	});
+
+	// add the tiles that aren't currently visible
+	/*
+	for (var x = 0; x < parseInt(_RAPHAEL.width); x += _TILESIZE) {
+	    for (var y = 0; y < parseInt(_RAPHAEL.height); y += _TILESIZE) {
+		var els = _RAPHAEL.getElementsByPoint(x,y);
+		//if (els.length != 0) // continue if there's at least one element
+		//continue;
+
+		if (els.length == 0){
+		    // add tile to the map coordinate equivalent of Raphael's screen coords x,y
+		    var mapX = _map_x + x;
+		    var mapY = _map_y + y;
+		    console.log("Adding new tile at ("+mapX+","+mapY+")");
+		    tile_new(mapX, mapY);
+		}else
+		    console.log(0);
+	    }
+	}
+	*/
+    }
+
+    // Update the metadata bar
+    /*
+    if (_mouse_state == _MOUSE_UP)
+	metabar_update(_map_x+_mouse_move_x, _map_y+_mouse_move_y);
+    else
+	metabar_update(_map_x, _map_y);
+    */
+    metabar_update(_map_x, _map_y);
+
+    _mouse_move_lastX = _mouse_move_x;
+    _mouse_move_lastY = _mouse_move_y;
+}
+
+function metabar_update(xval, yval){
+    /*
     var newx = Math.floor(_map_x*100/_TILESIZE)/100;
     var newy = Math.floor(_map_y*100/_TILESIZE)/100;
     var inner = newx+", "+newy;
+    */
 
-    /*
-    var fancyCoordsify = function(x){
-	return x/_TILESIZE*20/1000; // one pixel is 20m
+    var fancyCoordsify = function(x, obj){
+	var direction = x > 0 ? obj.pos : obj.neg;
+	x = Math.abs(x);
+
+	var toTwoPlaces = function(n){ return Math.floor(n*100)/100; }
+
+	var meters = x*_METERS_PER_PIXEL;
+	if (meters < 1000){
+	    return toTwoPlaces(meters)+"m "+direction;
+	}else{
+	    return toTwoPlaces(meters/1000)+"km "+direction;
+	}
     };
 
-    var x = fancyCoordsify(_map_x);
-    var y = fancyCoordsify(_map_y);
+    var x = fancyCoordsify(xval, { pos: "West",  neg: "East"  });
+    var y = fancyCoordsify(yval, { pos: "North", neg: "South" });
     
-    var inner = y+"km North, "+x+"km East";
-    */
+    var inner = y+", "+x;
 
     document.getElementById('mapPosition').innerHTML = "<pre>"+inner+"</pre>";
 }
