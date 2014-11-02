0420cb5a78594b2eb54a6ccc77a001de003dd2b8
Kenzie Togami <ket1999@gmail.com>
2014-11-01 21:26:25 -0700

    Let's try this thing

--- forums/js/app.js
+++ forums/js/app.js
@@ -1,8 +1,30 @@
 define(["jqui", "firebase"], function ($undef, Firebase) {
     var app = {};
     
+    var hooks = {};
+    app.hooks = hooks;
+    function generate_forum_topic(forum) {
+        return '<div id="' + forum + '" class="forum-topic"></div>';
+    }
+    hooks.add_forum = function (forum, lastForumName) {
+        var name = forum.name();
+        var fcontainer = $('#forums');
+        var topic = generate_forum_topic(name);
+        if (!lastForumName) {
+            // just dump it at the top
+            fcontainer.prepend(topic);
+        } else {
+            $(topic).insertAfter('#' + name);
+        }
+    };
+
+    function setup_firehooks(fb) {
+        fb.on('child_added', add_forum);
+    }
+
     app.init = function init() {
         app.fire = new Firebase("https://conworld.firebaseio.com").child("forums");
+        setup_firehooks(app.fire);
         console.log('initalized forums');
     };
 
