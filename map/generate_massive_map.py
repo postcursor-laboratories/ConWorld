3b8f0ed4b3d9aff54b1ea15d15cf3859fca6eb58
charlie <charliegreen@berkeley.edu>
2014-11-06 01:25:25 -0800

    Added a quick program for generating zoomed out and very large maps, for seeing large-scale terrain generation patterns

---
+++ map/generate_massive_map.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import mapgen
+
+xoffset = -512
+yoffset = -512
+scale = 80
+
+hm = mapgen._heightmapper
+mp = mapgen.transform_addAltitude(hm)
+
+def massive_map_pixel_func(x,y):
+    x += xoffset
+    y += yoffset
+
+    x *= scale
+    y *= scale
+
+    return mp(x,y)
+
+if __name__ == '__main__':
+    scale = int(sys.argv[1])
+    n = int(sys.argv[2])
+    print("generating %d square map with scale %d" % (n,scale))
+    print("estimated time: %ds" % (n**2/256**2*48))
+    mapgen.createPNG(massive_map_pixel_func, "massivemap.png", n)
