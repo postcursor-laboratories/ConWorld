f03399bfd62132adfcd554b914d57b02d8d20d5a
Kenzie Togami <ket1999@gmail.com>
2014-11-05 21:19:14 -0800

    bilinear interpolation right

--- map/value.py
+++ map/value.py
@@ -16,6 +16,35 @@ def replwfunc(o):
     elif isinstance(o, Sequence):
         o = o.__getitem__
     return o
+def bilinear_interpolation(x, y, points):
+    '''Interpolate (x,y) from values associated with four points.
+
+    The four points are a list of four triplets:  (x, y, value).
+    The four points can be in any order.  They should form a rectangle.
+
+        >>> bilinear_interpolation(12, 5.5,
+        ...                        [(10, 4, 100),
+        ...                         (20, 4, 200),
+        ...                         (10, 6, 150),
+        ...                         (20, 6, 300)])
+        165.0
+
+    '''
+    # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
+
+    points = sorted(points)               # order points by x, then by y
+    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
+
+    if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+        raise ValueError('points do not form a rectangle')
+    if not x1 <= x <= x2 or not y1 <= y <= y2:
+        raise ValueError('(x, y) not within the rectangle')
+
+    return (q11 * (x2 - x) * (y2 - y) +
+            q21 * (x - x1) * (y2 - y) +
+            q12 * (x2 - x) * (y - y1) +
+            q22 * (x - x1) * (y - y1)
+           ) / ((x2 - x1) * (y2 - y1) + 0.0)
 #------------------------------------------------------------------------------ 
 # Method 2 from http://lodev.org/cgtutor/randomnoise.html
 class ValueNoise:
@@ -42,24 +71,18 @@ class ValueNoise:
     def smooth_noise(self, x, y):
         """Returns the average value of the 4 neighbors of (x, y) from the
            noise array."""
-
-        fractX = abs(x - int(x))
-        fractY = abs(y - int(y))
-
         x1 = int(x)
         y1 = int(y)
-
         x2 = x1 - 1
         y2 = y1 - 1
 
         #Bilinear interpolation http://en.wikipedia.org/wiki/Bilinear_interpolation
-        value = 0.0
-        value += fractX       * fractY       * self.noise(x1, y1)
-        value += fractX       * (1 - fractY) * self.noise(x1, y2)
-        value += (1 - fractX) * fractY       * self.noise(x2, y1)
-        value += (1 - fractX) * (1 - fractY) * self.noise(x2, y2)
+        x1y1 = (x1, y1, self.noise(x1, y1))
+        x1y2 = (x1, y2, self.noise(x1, y2))
+        x2y1 = (x2, y1, self.noise(x2, y1))
+        x2y2 = (x2, y2, self.noise(x2, y2))
 
-        return value
+        return bilinear_interpolation(x, y, [x1y1, x1y2, x2y1, x2y2])
     def generate(self, x, y):
         """
         Generate dat value noise, boi
