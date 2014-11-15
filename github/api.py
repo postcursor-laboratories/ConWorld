1048c36a71574ca38ffe72d556ac6937d9bfe33b
Kenzie Togami <ket1999@gmail.com>
2014-11-15 12:47:01 -0800

    this nice api shall suffice

---
+++ github/api.py
@@ -0,0 +1,22 @@
+API_URL = "https://api.github.com"
+
+def get_url(frag):
+    if frag[0] == '/':
+        # fragment starts with /
+        if API_URL[-1] == '/':
+            # API_URL ends with it and we don't want //
+            frag = frag[1:]
+        else:
+            # okay to pass
+            pass
+    else:
+        # fragment does not start with /
+        if API_URL[-1] == '/':
+            # API_URL does, it will make /
+            pass
+        else:
+            # there will not be a slash, add one
+            frag = '/' + frag
+    return API_URL + frag
+
+__all__ = ["get_url"]
---
+++ github/ratelimit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..')) # top-level py
+import conlib
+import requests
+import datetime
+from github.api import get_url
+
+def get_raw_rate_json(request_auth=None):
+    req = requests.get(get_url('/rate_limit'), auth=request_auth)
+    req.raise_for_status()
+    json = req.json()
+    return json
+def get_rate_data(location='core', **kwargs):
+    data = get_raw_rate_json(**kwargs)
+    rldata = data['resources'][location]
+    return rldata
+mla = '%d %B %Y @ %I:%M:%S %p'
+def readable_date(millis_since_epoch):
+    # my english teacher wants this to be mla formatted
+    return datetime.datetime.fromtimestamp(millis_since_epoch).strftime(mla)
+def write_rate_limit():
+    corerate = get_rate_data()
+    print('Core rate data:', "{}/{}, resets on {}".format(corerate['remaining'],
+                                                         corerate['limit'],
+                                                         readable_date(corerate['reset'])))
+
+if __name__ == "__main__":
+    conlib.write_page(write_rate_limit)
