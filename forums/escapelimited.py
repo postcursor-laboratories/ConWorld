c76b565c256b9df7fc69db4469407827753ce63d
Kenzie Togami <ket1999@gmail.com>
2014-11-14 22:26:28 -0800

    escapelimited is better now

--- forums/escapelimited.py
+++ forums/escapelimited.py
@@ -3,37 +3,20 @@ import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..')) # top-level py
 import conlib
 import re
-from urlparse import urljoin
+import requests
+import cgi
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup, Comment
 from http.client import OK, BAD_REQUEST
-codeset = 'img, b, i, em, strong, strike, p, header'.split(', ')
 
-def limited_escape(string):
-    rjs = r'[\s]*(&#x.{1,7})?'.join(list('javascript:'))
-    rvb = r'[\s]*(&#x.{1,7})?'.join(list('vbscript:'))
-    re_scripts = re.compile('(%s)|(%s)' % (rjs, rvb), re.IGNORECASE)
-    validTags = 'p em i strong b u a h1 h2 h3 pre br img'.split()
-    validAttrs = 'href src width height'.split()
-    urlAttrs = 'href src'.split() # Attributes which should have a URL
-    soup = BeautifulSoup(string)
-    for comment in soup.findAll(text=lambda text: isinstance(text, Comment)):
-        # Get rid of comments
-        comment.extract()
-    for tag in soup.findAll(True):
-        if tag.name not in validTags:
-            tag.hidden = True
-        attrs = tag.attrs
-        tag.attrs = []
-        for attr, val in attrs:
-            if attr in validAttrs:
-                val = re_scripts.sub('', val) # Remove scripts (vbs & js)
-                if attr in urlAttrs:
-                    val = urljoin(base_url, val) # Calculate the absolute url
-                tag.attrs.append((attr, val))
+mdurl = 'https://api.github.com/markdown'
+markdownheaders = {'Accept': 'application/vnd.github.v3+json'}
 
-    return soup.renderContents().decode('utf8')
-def glidus_glue_code(ggcodestr):
-    glidus = '|'
+def github_markdown(unsafe):
+    unsafe = cgi.escape(unsafe) # no HTML
+    postdata = {'text': unsafe, 'mode': 'markdown'}
+    response = requests.post(mdurl, json=postdata, headers=markdownheaders)
+    return response.text
 
 inputkey = 'input'
 if __name__ == "__main__":
@@ -41,11 +24,12 @@ if __name__ == "__main__":
     code = OK
     data = ''
     input_ = ''
-    if not inputkey in form or not (input_ = form[inputkey]):
-        code = BAD_REQUEST
-        data = 'no data transmitted'
-    data = limited_escape(input_)
-    data = glidus_glue_code(data)
+    if not inputkey in form:
+        input_ = form[inputkey]
+        if not input_:
+            code = BAD_REQUEST
+            data = 'no data transmitted'
+    data = github_markdown(input_)
     conlib.write_status(code)
     print('Content-type: text/plain')
     conlib.end_headers()
